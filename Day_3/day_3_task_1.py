32c32a02028ea30127ea59ec8e1caf77fa541f94
Maxwell Barton <max_barton@Maxwells-MBP.lan>
2022-12-03 20:11:08 +0000

    Changed day_3_task_1 code

--- Day_3/day_3_task_1.py
+++ Day_3/day_3_task_1.py
@@ -1,6 +1,25 @@
+import string
 fname = "input.txt"
 
+code_dict = {}
+count = 1
+for s in string.ascii_lowercase:
+    code_dict[s] = count
+    count +=1
+for s in string.ascii_uppercase:
+    code_dict[s] = count
+    count +=1
+
+scores = []
 with open(fname) as f:
     for line in f:
         text = line.strip()
-        print(text)
+        text_len = int(len(text)/2)
+        r1 = text[:text_len]
+        r2 = text[text_len:]
+
+        code = set(r1).intersection(set(r2)) # item type
+        code = list(code)[0]
+        scores.append(code_dict[code])
+
+print(sum(scores))
